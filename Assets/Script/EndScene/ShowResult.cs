2040c047db3858187cd886b16118244a516df8fb
IwamotoHinata <86921212+IwamotoHinata@users.noreply.github.com>
2025-02-14 17:29:25 +0900

    a

---
+++ Assets/Script/EndScene/ShowResult.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ShowResult : MonoBehaviour
+{
+    [SerializeField] private TMP_Text resultText;
+
+
+    void Start()
+    {
+        int score = ScoreManager.Instance.GetScore();
+
+        resultText.text = "Your Score is : " + score.ToString();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Plant/CountdownTimer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+public class TimeCounter : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text countdownText;
+
+    [SerializeField]
+    private float countdownTime = 5f; // Countdown starts at 5 seconds
+
+    private bool isCountingDown = true;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isCountingDown)
+        {
+            countdownTime -= Time.deltaTime;
+            countdownTime = Mathf.Max(countdownTime, 0); // Ensure time doesn't go below 0
+
+            // Update the UI text
+            if (countdownText != null)
+            {
+                countdownText.text = countdownTime > 0
+                    ? $"Time: {countdownTime:F2}"
+                    : "The flower has wilted!";
+            }
+
+            // Stop counting and check flower status when the timer ends
+            if (countdownTime <= 0 && isCountingDown)
+            {
+                OnCountdownEnd();
+            }
+        }
+    }
+
+    public void OnCountdownEnd()
+    {
+        isCountingDown = false;
+        Debug.Log($"{gameObject.name}: Countdown has ended.");
+        countdownText.gameObject.SetActive(false);
+    }
+
+
+}
