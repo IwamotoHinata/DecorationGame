c3ffe74813281aaa588cf8199c1686c214e732f4
IwamotoHinata <c612101277@tokushima-u.ac.jp>
2025-02-22 17:25:30 +0900

    a

---
+++ Assets/Script/Plant/PlantCounter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlantCounter : MonoBehaviour
+{
+    [SerializeField] private PlantingZone[] _plantingZones;
+    private int _count = 0;
+    private int _maxCount;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _plantingZones = FindObjectsByType<PlantingZone>(FindObjectsSortMode.None);
+        _maxCount = _plantingZones.Length;
+    }
+
+    public void IncreaseCount()
+    {
+        _count++;
+        if (_count == _maxCount)
+        {
+            ScoreManager.Instance.IncreaseScore(25);
+        }
+    }
+
+    public void DecreaseCount()
+    {
+        if (_count == _maxCount)
+        {
+            ScoreManager.Instance.DecreaseScore(25);
+        }
+        _count--;
+    }
+}
